617110ec6ed455802482dcb15ee5fb1980a01998
nph17103 <nph17103@gmail.com>
2023-04-20 22:46:49 +0700

    added home controller

---
+++ src/main/java/com/example/springsocial2cloud2/SpringSocial2Cloud2Application.java
@@ -0,0 +1,13 @@
+package com.example.springsocial2cloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocial2Cloud2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocial2Cloud2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsocial2cloud2/SpringSocial2Cloud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsocial2cloud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocial2Cloud2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
